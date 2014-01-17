7d6a5bafba1ac311db34c1c6e398d44f793c87a6
Sam Richard <snugug@gmail.com>
2014-01-17 16:52:01 -0500

    Proper Versions

--- lib/singularity-extras.rb
+++ lib/singularity-extras.rb
@@ -6,6 +6,6 @@ require 'sassy-math'
 Compass::Frameworks.register("singularity-extras", :path => "#{File.dirname(__FILE__)}/..")
 
 module SingularityExtras
-  VERSION = "0.0.3"
-  DATE = "2012-04-24"
+  VERSION = "1.0.0.alpha.1"
+  DATE = "2013-01-17"
 end
