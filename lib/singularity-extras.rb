8a7d5958141279535dd1f6701f7334cbebb0063f
Sam Richard <snugug@gmail.com>
2014-02-11 16:36:11 -0500

    Version Bump

--- lib/singularity-extras.rb
+++ lib/singularity-extras.rb
@@ -5,6 +5,6 @@ require 'modular-scale'
 Compass::Frameworks.register("singularity-extras", :path => "#{File.dirname(__FILE__)}/..")
 
 module SingularityExtras
-  VERSION = "1.0.0.alpha.1"
-  DATE = "2013-01-17"
+  VERSION = "1.0.0.alpha.2"
+  DATE = "2013-02-11"
 end
